1f391a1469b13b5957d69e879dbc8eafc4f211ca
Lorin Beer <lorin.beer@gmail.com>
2014-10-29 15:48:32 -0700

    [#445] bin/phonegap points to phonegap cli module

--- bin/phonegap.js
+++ bin/phonegap.js
@@ -4,13 +4,13 @@
  * Module dependencies.
  */
 
-var CLI = require('../lib/cli');
+var PG_CLI = require('../lib/phonegapcli');
 
 /*!
  * Run the command-line client.
  */
 
-var cli = new CLI().argv(process.argv, function(e) {
+var cli = new PG_CLI().argv(process.argv, function(e) {
     // if we receive an error, then exit with an error status
     // if an exit code was attached to the error, then use it
     // otherwise default to 1.
