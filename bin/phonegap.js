1b5edebca70b3a9d831965be207b0c045d350e32
Lorin Beer <lorin.beer@gmail.com>
2014-10-29 15:50:20 -0700

    [#445] second version of bin/phonegap, calls cli core module

--- bin/phonegap.js
+++ bin/phonegap.js
@@ -4,13 +4,13 @@
  * Module dependencies.
  */
 
-var PG_CLI = require('../lib/phonegapcli');
+var core = require('../src/cli/core');
 
 /*!
  * Run the command-line client.
  */
 
-var cli = new PG_CLI().argv(process.argv, function(e) {
+var cli = new core()(process.argv, function(e) {
     // if we receive an error, then exit with an error status
     // if an exit code was attached to the error, then use it
     // otherwise default to 1.
