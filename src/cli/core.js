eb3b25b32079f9005f14c68c85926b9bdc50ed4f
Lorin Beer <lorin.beer@gmail.com>
2014-10-29 15:45:41 -0700

    [#443] rewrite of 'argv' module, now called cli/core
    same basic principle, core is the main entry point and coordinates between different flows of control for the commands, just with half the code

---
+++ src/cli/core.js
@@ -0,0 +1,55 @@
+/**
+ * Module Dependencies
+ */
+var mini = require('minimist');
+
+var phonegap = require('../phonegap');
+var phonegapify = require('../phonegapify');
+
+
+/**
+ *
+ */
+parseOptions = {
+    boolean : [ 'd', 'device',
+                'e', 'emulator',
+                'd', 'verbose',
+                'v', 'version',
+                'h', 'help',
+                'autoreload',
+                'localtunnel'
+              ],
+    default : {
+        'autoreload' : true,
+        'localtunnel': false
+    }
+};
+
+
+/**
+ * delegates to the appropriate handler for the arguments received
+ * @param argv {Object} should be the raw CL arguments
+ * @param callback {Function} optional callback parameter
+ * @returns - undefined
+ */
+function CLICore(argv, callback) {
+    var parsed,
+        command;
+
+    callback = callback || function () {};
+
+    // slice off 'node */phonegap'
+    parsed = mini(argv.slice(2), parseOptions);
+
+    if (phonegap.subcommands.indexOf(parsed._[0]) > -1 && typeof phonegap[parsed._[0]] === 'function') {
+        // execute phonegap implementation of the subcommand    
+    } else {
+        phonegapify(parsed, callback);
+    }
+};
+
+
+/**
+ * Module Exports
+ */
+module.exports = CLICore;
