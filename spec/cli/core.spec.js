1bdbbc901f36464ee2111d39e9da646c05222b83
Lorin Beer <lorin.beer@gmail.com>
2014-10-30 17:19:29 -0700

    [4.0.0] updated spec, now passing, uses rewire module for mocking dependencies

--- spec/cli/core.spec.js
+++ spec/cli/core.spec.js
@@ -1,25 +1,30 @@
 var test = require('tap').test;
 var sinon = require('sinon');
+var rewire = require('rewire');
 
-var Core = require('../../src/cli/core');
-var phonegapify = require('../../src/cli/phonegapify');
-var mini = require('minimist');
+var Core = rewire('../../src/cli/core');
 
+
+var phonegapify = sinon.spy();
+
+function beforeEach() {
+    Core.__set__('phonegapify',   phonegapify);
+};
 
 var base_argv = [ 'node', 'something/something/phonegap' ];
 
+beforeEach();
 test("CLI Core Module", function (t) {
     t.plan(2);
 
     t.type(Core, 'function', "should export a function");
 
+        beforeEach();
     t.test("should call phonegapify module if command is not overwritten", function (t) {
+
         t.plan(1);
-        var gapifyspy = sinon.spy(phonegapify);        
-
         Core(base_argv, sinon.spy());
-
-        t.equal(gapifyspy.callCount, 0, "call count should be exactly 1")
+        t.equal(phonegapify.callCount, 1, "call count should be exactly 1")
 
     });
 
