62f27b081fb4c4fa91756b14563cfc90a624c860
Lorin Beer <lorin.beer@gmail.com>
2014-10-31 17:25:28 -0700

    [4.0.0][#447] additional e2e test for create command
    updated staging, doen't alter cwd after staging is complete

--- e2e/create.e2e.js
+++ e2e/create.e2e.js
@@ -1,16 +1,22 @@
 /**
- *
+ * tesing dependencies
  */
 var test = require('tap').test;
 var sinon = require('sinon');
 var rewire = require('rewire');
 
+/**
+ * staging dependencies
+ */
 var fs = require('fs.extra');
 var path = require('path');
 var async = require('async');
 var shell = require('shelljs');
 var spawn = require('child_process').spawn;
 
+/**
+ * Paths
+ */
 var stagedir = path.join('.', 'staged');
 var cordovapath = path.join('..', 'node_modules', 'cordova');
 
@@ -19,23 +25,57 @@ var cordovapath = path.join('..', 'node_modules', 'cordova');
 /**
  *
  */
-function clean (callback) {
+test("phonegap create", function (t) {
+    t.plan(1);
+    
+    t.test("when created with no options", function (t) {
+        t.plan(5);
+        stage(['create', stagedir], function () {
+            // beware of exists
+            fs.exists(stagedir, function (exists) {
+                t.ok(exists, "cordova project directory exists");
+            });
+
+            fs.exists(path.join(stagedir, 'config.xml'), function (exists) {
+                t.ok(exists, "project has a config.xml file");
+            });
+
+            fs.exists(path.join(stagedir, 'www'), function (exists) {
+                t.ok(exists, "project has a www/");
+            });
+
+            fs.exists(path.join(stagedir, 'www'), function (exists) {
+                t.ok(exists, "project has a platforms/");
+            });
+
+            fs.exists(path.join(stagedir, 'www'), function (exists) {
+                t.ok(exists, "project has a hooks/");
+            });
+        });
+    });
+});
+
+
+/**
+ * teardown the stage after testing
+ */
+function teardown (callback) {
     fs.rmrf(stagedir, function (err) {
         if (err) {
-            console.err("Error removing directory " + stagedir); 
+            console.error("Error removing directory " + stagedir); 
         }
         callback();
     }); 
 }
 
 /**
- *
+ * set up the stage for testing
  */
 function stage (args, cb) {
     var origwd = process.cwd();
 
     async.series([
-        clean,
+        teardown,
         function (callback) {
             fs.mkdir(stagedir, function (err) {
                 callback();
@@ -43,31 +83,14 @@ function stage (args, cb) {
         },
         function (callback) {
             process.chdir(cordovapath);
-            spawn('cordova', args, {cwd: origwd, stdio:'inherit'}); 
+            child = spawn('cordova', args, {cwd: origwd, stdio:'inherit'});
+
+            child.on('exit', function(err){
+                process.chdir(origwd);
+                cb();
+            });
         } 
-    ], function (e, result) {
+    ], function (err, result) {
+        console.error('staging error');
     });
 };
-
-
-/**
- *
- */
-test("Project Creation", function(t) {
-    t.plan(3);
-
-    stage(['create', stagedir], function () {});
-
-    // beware of exists
-    fs.exists(stagedir, function (exists) {
-        t.ok(exists, "cordova project directory exists");
-    });
-
-    fs.exists(path.join(stagedir, 'config.xml'), function (exists) {
-        t.ok(exists, "project has a config.xml file");
-    });
-
-    fs.exists(path.join(stagedir, 'www'), function (exists) {
-        t.ok(exists, "project has a www/");
-    });
-});
