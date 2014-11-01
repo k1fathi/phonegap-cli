d961799f9c28107ce2158416d2b815dd154eb110
Lorin Beer <lorin.beer@gmail.com>
2014-10-31 17:25:28 -0700

    [4.0.0] updated e2e path staging directory so tap/test runner does not attempt to run js inside staged projects

--- e2e/create.e2e.js
+++ e2e/create.e2e.js
@@ -11,44 +11,82 @@ var rewire = require('rewire');
 var fs = require('fs.extra');
 var path = require('path');
 var async = require('async');
-var shell = require('shelljs');
 var spawn = require('child_process').spawn;
 
 /**
  * Paths
  */
-var stagedir = path.join('.', 'staged');
+var stagedir = path.join('..', 'stage');
+var projpath = path.join(stagedir, 'test_project');
 var cordovapath = path.join('..', 'node_modules', 'cordova');
 
 
-
 /**
- *
+ * TESTS
  */
 test("phonegap create", function (t) {
-    t.plan(1);
-    
+    t.plan(3);
+
+    /* phonegap create */
+    t.test("when invoked with no arguments", function (t) {
+        t.plan(1);
+
+        stage(['create'], function () {
+            fs.exists(projpath, function (exists) {
+                t.ok(!exists, "cordova project directory should not be created without a path");
+            });
+        });
+    });
+
+    /* phonegap create path */ 
     t.test("when created with no options", function (t) {
         t.plan(5);
-        stage(['create', stagedir], function () {
+        stage(['create', projpath], function () {
             // beware of exists
-            fs.exists(stagedir, function (exists) {
+            fs.exists(projpath, function (exists) {
                 t.ok(exists, "cordova project directory exists");
             });
 
-            fs.exists(path.join(stagedir, 'config.xml'), function (exists) {
+            fs.exists(path.join(projpath, 'config.xml'), function (exists) {
                 t.ok(exists, "project has a config.xml file");
             });
 
-            fs.exists(path.join(stagedir, 'www'), function (exists) {
+            fs.exists(path.join(projpath, 'www'), function (exists) {
                 t.ok(exists, "project has a www/");
             });
 
-            fs.exists(path.join(stagedir, 'www'), function (exists) {
+            fs.exists(path.join(projpath, 'www'), function (exists) {
                 t.ok(exists, "project has a platforms/");
             });
 
-            fs.exists(path.join(stagedir, 'www'), function (exists) {
+            fs.exists(path.join(projpath, 'www'), function (exists) {
+                t.ok(exists, "project has a hooks/");
+            });
+        });
+    });
+
+    /* phonegap create path id name */
+    t.test("with path id name arguments", function (t) {
+        t.plan(5);
+        stage(['create', projpath], function () {
+            // beware of exists
+            fs.exists(projpath, function (exists) {
+                t.ok(exists, "cordova project directory exists");
+            });
+
+            fs.exists(path.join(projpath, 'config.xml'), function (exists) {
+                t.ok(exists, "project has a config.xml file");
+            });
+
+            fs.exists(path.join(projpath, 'www'), function (exists) {
+                t.ok(exists, "project has a www/");
+            });
+
+            fs.exists(path.join(projpath, 'www'), function (exists) {
+                t.ok(exists, "project has a platforms/");
+            });
+
+            fs.exists(path.join(projpath, 'www'), function (exists) {
                 t.ok(exists, "project has a hooks/");
             });
         });
