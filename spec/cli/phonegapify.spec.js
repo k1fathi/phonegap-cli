937e0e51824af59d0fada814538432cfb9947a35
Lorin Beer <lorin.beer@gmail.com>
2014-10-31 17:25:28 -0700

    [4.0.0][#446] basic behaviour of phonegapify module specced out
    phonegapify now correctly executes the callback once the child process has exited

--- spec/cli/phonegapify.spec.js
+++ spec/cli/phonegapify.spec.js
@@ -3,21 +3,27 @@ var test = require('tap').test;
 var sinon = require('sinon');
 var rewire = require('rewire');
 
-//
+/* rewire module under test */
 var phonegapify = rewire('../../lib/cli/phonegapify');
 
-
 var chdirspy = sinon.spy();
+var spawnspy = sinon.stub().returns({on: function (code, callback) {callback()}});
 var callback = sinon.spy();
 
+/**
+ *
+ */
 function beforeEach() {
     phonegapify.__set__('process.chdir', chdirspy);
+    phonegapify.__set__('spawn', spawnspy);
 };
 
 var base_argv = [ 'node', 'something/something/phonegap' ];
 var cmd_argv = [ 'create', 'path/to/app' ];
 
-
+/**
+ *
+ */
 beforeEach();
 test("PhoneGapify Module", function (t) {
     t.plan(3);
@@ -25,20 +31,24 @@ test("PhoneGapify Module", function (t) {
     t.type(phonegapify, 'function', "should export a function");
 
     beforeEach();
-    t.test("", function (t) {
-        t.plan(2);
+    t.test("invoking phonegapify with an argument list", function (t) {
+        t.plan(5);
 
         // given        
         phonegapify(cmd_argv, callback);
 
         // then
-
-        t.equal(chdirspy.args[0][0], phonegapify.__get__('cordovapath'),"should change the worknig directory");
+        t.equal(chdirspy.args[0][0], phonegapify.__get__('cordovapath'),"should change the working directory");
         t.equal(chdirspy.callCount, 1, "should change the working directory exactly once");
-    });
 
+        t.equal(spawnspy.callCount, 1, "should call child_process.spawn");
+        t.equal(spawnspy.args[0][0], 'cordova', "should spawn with 'cordova' as first argument");
+        t.equal(spawnspy.args[0][1], cmd_argv, "should call spawn with unmoddified arguments");
+
+    });
+    
     beforeEach();
-    t.test("", function (t) {
+    t.test("if cordova is missing", function (t) {
         t.plan(1);
 
         // given        
@@ -48,3 +58,6 @@ test("PhoneGapify Module", function (t) {
         t.equal(chdirspy.callCount, 2, "");
     });
 });
+
+
+
