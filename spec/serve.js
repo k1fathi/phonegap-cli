80ee20b6de1ae05c8371b73d99be1fbe5c26300d
Lorin Beer <lorin.beer@gmail.com>
2014-11-05 12:48:58 -0800

    [4.0.0][#460] 5 additional tests covering registering of listeners on the Server module

--- spec/serve.js
+++ spec/serve.js
@@ -18,12 +18,14 @@ function beforeEach() {
     
     /* instantiate spies*/
     serverstub = {};
-    serverstub.listen = sinon.stub().returns(serverret);
+    serverstub.listen = sinon.stub();
     cdspy = sinon.stub().returns(true);
     
     onstub = sinon.stub();
     serverret = {on: onstub};
     onstub.returns(serverret);
+    serverstub.listen.returns(serverret);
+
     callbackspy = sinon.spy();
 
     // hot mock injection
@@ -46,7 +48,7 @@ var valid_options = {port: 3000, autoreload: true, localtunnel: false};
 /* TESTS */
 beforeEach();
 test("Serve Module", function (t) {
-    t.plan(3);
+    t.plan(5);
 
     /* trivial state test */
     t.type(serve, 'function', "should export a function");
@@ -54,7 +56,7 @@ test("Serve Module", function (t) {
     /* invoking with valid arguments */
     beforeEach();
     t.test("when invoking serve with a valid argument list", function (t) {
-        t.plan(5);
+        t.plan(6);
 
         // given        
         serve(valid_options, callbackspy);
@@ -63,8 +65,9 @@ test("Serve Module", function (t) {
         t.ok(cdspy.calledOnce, "should attempt to change the working directory to the project directory");
         t.equal(serverstub.listen.callCount, 1, "should call the server module once");
         t.type(serverstub.listen.args[0][0], 'object', "should invoke the server module with options");
-        t.equal(onstub.callCount, 0, "should register error, log and complete event listeners for server events");
+        t.equal(onstub.callCount, 3, "should register event listeners for server events");
         t.ok(callbackspy.calledOnce, "should call the callback passed in as a parameter");
+        t.equal(callbackspy.args[0].length, 0, "should call the callback with no arguments");
 
         afterEach();    
     });
@@ -72,7 +75,7 @@ test("Serve Module", function (t) {
     /* cd fail case  */
     beforeEach();
     t.test("when invoking serve with failing project.cd attempt", function (t) {
-        t.plan(2);
+        t.plan(3);
         
         // given
         cdspy.returns(false);
@@ -81,9 +84,32 @@ test("Serve Module", function (t) {
         // then
         t.equal(cdspy.callCount, 1, "should attempt to call the project.cd utility function");
         t.ok(callbackspy.calledOnce, "should execute the callback passed in as a parameter");
-///        t.equal(callbackspy.args[0][
+        t.type(callbackspy.args[0][0], 'Error', "should pass an error back with the callback");
 
         afterEach();
     });
-    afterEach();
+
+    beforeEach()
+    t.test("when executed, should register event handlers", function (t) {
+        beforeEach();
+        t.plan(4);
+
+        // given
+        serve(valid_options, callbackspy);
+
+        //then
+        t.equal(onstub.callCount, 3, "should register 3 handers");
+        t.ok(onstub.calledWithMatch('error'), "should register an 'error' event handler on the return object form Server.listen");
+        t.ok(onstub.calledWithMatch('log'), "should register an 'log' event handler on the return object form Server.listen");
+        t.ok(onstub.calledWithMatch('complete'), "should register an 'complete' event handler on the return object form Server.listen");
+        afterEach();
+    });
+
+    /* on 'error' */
+    beforeEach();
+    t.test("", function (t) {
+        t.plan(1);
+        t.ok(true, "true");
+        afterEach();
+    });
 });
