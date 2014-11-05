cb8d611345e6297c6eb75bc491458309b6a3b5a4
Lorin Beer <lorin.beer@gmail.com>
2014-11-05 12:01:59 -0800

    [4.0.0][#460] additonal tests for spec/serve
    found issue with cleanup function
    comment changed in serve module

--- spec/serve.js
+++ spec/serve.js
@@ -7,49 +7,80 @@ var rewire = require('rewire');
 var serve = rewire('../lib/cli/serve');
 
 /* define spies */
-//var listenspy = sinon.stub().returns({on:sinon.spy()});
-var serverstub = {};
-var serveonspy = sinon.spy();
-var cdspy = sinon.stub().returns(true);
-var callback = sinon.spy();
-
-//var onstub = { on: function() { return onstub; }}
-var onstub = sinon.stub();
-var serverret = { on: onstub} 
-onstub.returns(serverret);
+var serverstub; 
+var cdspy; 
+var callbackspy; 
+var onstub;
+var serverret;
 
 /* setup spies/stubs/mocks */
 function beforeEach() {
+    
+    /* instantiate spies*/
+    serverstub = {};
     serverstub.listen = sinon.stub().returns(serverret);
+    cdspy = sinon.stub().returns(true);
+    
+    onstub = sinon.stub();
+    serverret = {on: onstub};
+    onstub.returns(serverret);
+    callbackspy = sinon.spy();
+
+    // hot mock injection
     serve.__set__('server', serverstub);
     serve.__set__('project.cd', cdspy);
 };
 
+/* teardown spies/stubs/mocks */
+function afterEach() {
+    cdspy.reset();
+    serve.__get__('server.listen').reset();
+    serve.__get__('project.cd').reset();
+};
+
+/* fixtures */
 var valid_options = {port: 3000, autoreload: true, localtunnel: false};
 
 
 /* TESTS */
 beforeEach();
 test("Serve Module", function (t) {
-    t.plan(2);
+    t.plan(3);
 
     /* trivial state test */
     t.type(serve, 'function', "should export a function");
 
     /* invoking with valid arguments */
     beforeEach();
-    t.test("invoking serve with a valid argument list", function (t) {
-        t.plan(4);
+    t.test("when invoking serve with a valid argument list", function (t) {
+        t.plan(5);
 
         // given        
-        serve(valid_options, callback);
+        serve(valid_options, callbackspy);
 
         // then
         t.equal(cdspy.callCount, 1, "should attempt to change the working directory to the project directory");
         t.equal(serverstub.listen.callCount, 1, "should call the server module once");
         t.type(serverstub.listen.args[0][0], 'object', "should invoke the server module with options");
-        t.equal(onstub.callCount, 3, "should register error, log and complete event listeners for server events");
-//        t.equal(spawnspy.args[0][0], 'cordova', "should spawn with 'cordova' as first argument");
-//        t.equal(spawnspy.args[0][1], cmd_argv, "should call spawn with unmoddified arguments");
+        t.equal(onstub.callCount, 0, "should register error, log and complete event listeners for server events");
+        t.equal(callbackspy.callCount, 1, "should call the callback passed in as a parameter");
+
+        afterEach();    
     });
+
+    /* cd fail case  */
+    beforeEach();
+    t.test("when invoking serve with failing project.cd attempt", function (t) {
+        t.plan(1);
+        
+        // given
+        cdspy.returns(false);
+        serve(valid_options, callbackspy);
+        
+        // then
+        t.equal(cdspy.callCount, 1, "should attempt to call the project.cd utility function");
+
+        afterEach();
+    });
+    afterEach();
 });
